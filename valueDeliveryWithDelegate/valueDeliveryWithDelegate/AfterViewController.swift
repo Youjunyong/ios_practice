e54f26bbaad013aa7adda9034cc9db2b2f3ffe09
you junyong <hellgey777@naver.com>
2021-07-31 14:41:49 +0900

    delegate practice

---
+++ valueDeliveryWithDelegate/valueDeliveryWithDelegate/AfterViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AfterViewController.swift
+//  valueDeliveryWithDelegate
+//
+//  Created by 유준용 on 2021/07/31.
+//
+
+import UIKit
+
+class AfterViewController: UIViewController {
+
+    var delegate : SendData?
+    
+    
+    @IBOutlet weak var inputField: UITextField!
+    @IBAction func ChangeBtn(_ sender: Any) {
+        delegate?.send(self, Input: inputField.text)
+        
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.inputField.delegate = self
+        
+    }
+
+
+}
+extension AfterViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        delegate?.send(self, Input: inputField.text)
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+        return true
+    }
+}
---
+++ valueDeliveryWithDelegate/valueDeliveryWithDelegate/DelegateProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  DelegateProtocol.swift
+//  valueDeliveryWithDelegate
+//
+//  Created by 유준용 on 2021/07/31.
+//
+
+import UIKit
+
+protocol SendData {
+    func send(_ vc: UIViewController, Input value : String?)
+}
