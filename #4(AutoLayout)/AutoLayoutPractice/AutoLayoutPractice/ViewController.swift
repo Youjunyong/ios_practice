404eef75d500e7ef55f4c146cf17d3834a7320f3
you junyong <hellgey777@naver.com>
2021-09-09 12:03:16 +0900

    StatusBar

--- #4(AutoLayout)/AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
+++ #4(AutoLayout)/AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -13,13 +13,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var profileImage: UIImageView!
     
     
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return  .lightContent
+    }
+
+    
     
     @IBOutlet weak var cancel: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        print("displayName:" + Bundle.displayName)
+        print(Bundle.appVersion)
+        print(Bundle.appBuildVersion)
+        print(Bundle.bundleIdentifier)
         let background = UIImage(named: "background_1")
         backgroundImage.contentMode = .scaleAspectFill
         backgroundImage.image = background
@@ -27,7 +34,36 @@ class ViewController: UIViewController {
         profileImage.layer.cornerRadius = profileImage.frame.width / 2.4
         profileImage.clipsToBounds = true
     }
+    
 
-
+}
+extension Bundle{
+    class var displayName : String{
+        if let value = Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String{
+            return value
+        }
+        return ""
+    }
+    
+    class var appVersion: String{
+        if let value = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            return value
+        }
+        return ""
+    }
+    
+    class var appBuildVersion: String{
+        if let value = Bundle.main.infoDictionary?["CFBundleVersion"] as? String{
+            return value
+        }
+        return ""
+    }
+    
+    class var bundleIdentifier: String{
+        if let value = Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String {
+            return value
+        }
+        return ""
+    }
 }
 
