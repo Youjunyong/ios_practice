2579ec3a87a1fd107733ace2db8712bf78c9b3e7
you junyong <hellgey777@naver.com>
2022-02-22 14:22:36 +0900

    Frame, bounds diff

--- 4(FrameAndBounds)/Pract/ViewController.swift
+++ 4(FrameAndBounds)/Pract/ViewController.swift
@@ -11,29 +11,29 @@ import UIKit
 class ViewController: UIViewController {
 
     var flag = 1
-    
-
-    lazy var testFrame: UIView = {
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.text = "*blue.frame: \(blue.frame.origin)\n*red.frame:\(red.frame.origin)\n*blue.bounds: \(blue.bounds.origin)\n*red.bounds:\(red.bounds.origin)"
+        return label
+    }()
+    lazy var blue: UIView = {
         let view = UIView()
         view.frame = CGRect(x: 100, y: 100, width: 300, height: 300)
-        
         view.backgroundColor = .blue
         return view
     }()
-    lazy var testBounds: UIView = {
+    
+    lazy var red: UIView = {
         let view = UIView()
         view.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
-//        view.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
         view.backgroundColor = .red
         return view
     }()
-    lazy var testBounds2: UIView = {
-        let view = UIView()
-        view.frame = CGRect(x: 130, y: 140, width: 30, height: 30)
-//        view.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
-        view.backgroundColor = .brown
-        return view
-    }()
+    
+
+    
     lazy var toggleButton: UIButton = {
         let btn = UIButton()
         btn.translatesAutoresizingMaskIntoConstraints = false
@@ -41,51 +41,38 @@ class ViewController: UIViewController {
         btn.setTitle("Toggle", for: .normal)
         btn.backgroundColor = .brown
         btn.titleLabel?.textColor = .red
-        
         return btn
-        
     }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-        
-        
-        print(view.superview)
-        DispatchQueue.main.async {
-            print(1)
-            print(2)
-            print(3)
-        }
-//        DispatchQueue.main.sync {
-//            print("a")
-//            print("b")
-//            print("c")
-//        }
-        
         // Do any additional setup after loading the view.
     }
 
     @objc func toggle(_: UIButton){
         if self.flag == 1{
             self.flag = 0
-            testFrame.bounds = CGRect(x: 0, y: 100, width: 300, height: 300)
-            print("flag == 1 , frame: ",testBounds.frame)
+            blue.bounds = CGRect(x: 0, y: 0, width: 300, height: 300)
+            
         }else{
             self.flag = 1
-            testFrame.bounds = CGRect(x: 0, y: 0, width: 300, height: 300)
-            print("flag == 2 , frame: ",testBounds.frame)
+            blue.bounds = CGRect(x: 100, y: 100, width: 300, height: 300)
         }
+        label.text = "*blue.frame: \(blue.frame.origin)\n*red.frame:\(red.frame.origin)\n*blue.bounds: \(blue.bounds.origin)\n*red.bounds:\(red.bounds.origin)"
 
     }
     private func configureUI(){
-        
-        view.addSubview(testFrame)
-        testFrame.addSubview(testBounds)
+        view.addSubview(blue)
+        blue.addSubview(red)
         view.addSubview(toggleButton)
-        testBounds.addSubview(testBounds2)
+
+        view.addSubview(label)
         NSLayoutConstraint.activate([
             toggleButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            toggleButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100)
+            toggleButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
 
---
+++ AutoScrollBanner/AutoScrollBanner/BannerCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  BannerCollectionViewCell.swift
+//  AutoScrollBanner
+//
+//  Created by 유준용 on 2022/02/02.
+//
+
+import UIKit
+
+class BannerCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var cellUIView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+
+}
